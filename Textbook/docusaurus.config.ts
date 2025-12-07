b9094b117364db1c94a15c0b6417fba878a87f4c
mutahirshah11 <mutahirhps@gmail.com>
2025-12-07 23:19:26 +0500

    Configure GitHub Pages deployment

--- Textbook/docusaurus.config.ts
+++ Textbook/docusaurus.config.ts
@@ -39,15 +39,15 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://Ragtextbook177.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // For GitHub pages deployment, it is '/<repository-name>/'
+  baseUrl: '/Tbook/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'mutahirshah11', // Usually your GitHub org/user name.
+  projectName: 'Tbook', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenAnchors: 'throw',
